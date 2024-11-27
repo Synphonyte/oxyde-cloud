c10b6e771f059734cda113985f2d6332ecb1ce92
Maccesch <maccesch@synphonyte.com>
2024-11-26 20:11:17 -0500

    added target to build command

--- leptos-cloud-cli/src/commands/build.rs
+++ leptos-cloud-cli/src/commands/build.rs
@@ -2,7 +2,14 @@ use cargo_leptos::config::Commands::Build;
 use cargo_leptos::config::{Cli, Opts};
 use cargo_leptos::run;
 
-pub async fn build(cargo_leptos_opts: Opts) -> anyhow::Result<()> {
+pub async fn build(mut cargo_leptos_opts: Opts) -> anyhow::Result<()> {
+    let mut cargo_args = cargo_leptos_opts.bin_cargo_args.unwrap_or_default();
+    cargo_args.push("--target".to_string());
+    cargo_args.push("x86_64-unknown-linux-gnu".to_string());
+    cargo_leptos_opts.bin_cargo_args = Some(cargo_args);
+
+    // std::env::set_var("LEPTOS_BIN_CARGO_COMMAND", "cross");
+
     run(Cli {
         manifest_path: None,
         log: vec![],
