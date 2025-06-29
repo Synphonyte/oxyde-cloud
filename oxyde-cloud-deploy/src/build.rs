c76878e19c7ab0501b8f9c7675eeee977fd6ffd1
matthias <matthias@synphonyte.com>
2025-06-29 12:01:18 +0100

    without bin target for now

--- oxyde-cloud-deploy/src/build.rs
+++ oxyde-cloud-deploy/src/build.rs
@@ -5,13 +5,16 @@ use cargo_leptos::run;
 const BUILD_TARGET: &'static str = "x86_64-unknown-linux-gnu";
 
 pub async fn build(mut cargo_leptos_opts: Opts) -> anyhow::Result<()> {
-    let mut cargo_args = cargo_leptos_opts.bin_cargo_args.unwrap_or_default();
-    cargo_args.push("--target".to_string());
-    cargo_args.push(BUILD_TARGET.to_string());
-    cargo_leptos_opts.bin_cargo_args = Some(cargo_args);
+    // let mut cargo_args = cargo_leptos_opts.bin_cargo_args.unwrap_or_default();
+    // cargo_args.push("--target".to_string());
+    // cargo_args.push(BUILD_TARGET.to_string());
+    // cargo_leptos_opts.bin_cargo_args = Some(cargo_args);
 
-    println!("bin args: {:#?}", cargo_leptos_opts.bin_cargo_args.clone().unwrap());
-    
+    println!(
+        "bin args: {:#?}",
+        cargo_leptos_opts.bin_cargo_args.clone().unwrap()
+    );
+
     cargo_leptos_opts.release = true;
 
     run(Cli {
@@ -19,5 +22,6 @@ pub async fn build(mut cargo_leptos_opts: Opts) -> anyhow::Result<()> {
         log: vec![],
         command: Build(cargo_leptos_opts),
     })
-    .await.map_err(|e| anyhow::anyhow!(e))
+    .await
+    .map_err(|e| anyhow::anyhow!(e))
 }
