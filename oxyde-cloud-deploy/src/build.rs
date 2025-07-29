ae0a6654ff5884e9a3cf9e127423c293941dcae2
matthias <matthias@synphonyte.com>
2025-07-29 19:18:39 +0100

    Update build.rs

--- oxyde-cloud-deploy/src/build.rs
+++ oxyde-cloud-deploy/src/build.rs
@@ -2,13 +2,13 @@ use cargo_leptos::config::Commands::Build;
 use cargo_leptos::config::{Cli, Opts};
 use cargo_leptos::run;
 
-const BUILD_TARGET: &'static str = "x86_64-unknown-linux-gnu";
+const BUILD_TARGET: &'static str = "x86_64-unknown-linux-musl";
 
 pub async fn build(mut cargo_leptos_opts: Opts) -> anyhow::Result<()> {
-    // let mut cargo_args = cargo_leptos_opts.bin_cargo_args.unwrap_or_default();
-    // cargo_args.push("--target".to_string());
-    // cargo_args.push(BUILD_TARGET.to_string());
-    // cargo_leptos_opts.bin_cargo_args = Some(cargo_args);
+    let mut cargo_args = cargo_leptos_opts.bin_cargo_args.unwrap_or_default();
+    cargo_args.push("--target".to_string());
+    cargo_args.push(BUILD_TARGET.to_string());
+    cargo_leptos_opts.bin_cargo_args = Some(cargo_args);
     
     run(Cli {
         manifest_path: None,
