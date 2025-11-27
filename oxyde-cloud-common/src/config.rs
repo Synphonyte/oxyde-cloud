632932f35b5a061b7af611dcede654e7f9c678d4
maccesch <maccesch@synphonyte.com>
2025-11-27 08:47:58 +0100

    fix

--- oxyde-cloud-common/src/config.rs
+++ oxyde-cloud-common/src/config.rs
@@ -20,7 +20,7 @@ pub struct AppConfig {
 }
 
 impl AppConfig {
-    pub const ALLOWED_CHARS: [char; _] = [
+    pub const ALLOWED_CHARS: [char; 37] = [
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
         's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
         '-',
